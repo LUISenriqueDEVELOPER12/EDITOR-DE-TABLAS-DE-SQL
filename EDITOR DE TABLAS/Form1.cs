d43ddaa4d322c8a783f0f93301e502476241c65b
Luise <Luise@LAPTOP-9A6L4Q4>
2022-07-31 09:55:28 -0500

    Agregar archivos de proyecto.

---
+++ EDITOR DE TABLAS/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+
+namespace EDITOR_DE_TABLAS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
